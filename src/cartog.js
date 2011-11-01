261f603dc35f569ee137f83fa8b43b566723dc41
Jarod Luebbert <jarodluebbert@gmail.com>
2011-10-31 20:50:02 -0700

    Removed jshint warnings, resizing now works

--- src/cartog.js
+++ src/cartog.js
@@ -17,9 +17,37 @@ var Cartog = new Class({
       x:this.x, 
       y:this.y,
       width:this.width,
-      height:this.height,
+      height:this.height
     });
     this.addLayer('background');
+  },
+  setWidth: function(width) {
+    this.width = width;
+    this.updateTileSize();
+    this.resizeAllLayers();
+  },
+  setHeight: function(height) {
+    this.height = height;
+    this.updateTileSize();
+    this.resizeAllLayers();
+  },
+  setRows: function(rows) {
+    this.rows = rows;
+    this.updateTileSize();
+    this.resizeAllLayers();
+  },
+  setColumns: function(columns) {
+    this.columns = columns;
+    this.updateTileSize();
+    this.resizeAllLayers();
+  },
+  resizeAllLayers: function() {
+    var self = this;
+    self.map.frame.width = self.width;
+    self.map.frame.height = self.height;
+    self.map.removeAllSublayers();
+    self.updateTileSize();
+    self.addLayer('background');
   },
   updateTileSize: function() {
     this.tileWidth = this.width / this.columns;
@@ -27,37 +55,34 @@ var Cartog = new Class({
   },
   addLayer: function(name) {
     var layer = UILayer({
-      x: 0,
-      y: 0,
-      width: this.width,
-      height: this.height,
+      anchor: 'top right bottom left',
       tag: name,
-      zPosition: 1,
+      zPosition: 1
     });
-    if (this.map.sublayers.length != 0) {
+    if (this.map.sublayers.length !== 0) {
       layer.opacity = 0.5;
-    };
+    }
+    this.addTiles(layer);
     this.map.addSublayer(layer);
-    this.addTiles();
     this.bindLayerEvents();
   },
-  addTiles: function() {
+  addTiles: function(layer) {
     for (var x = 0; x < this.columns; x++) {
       for (var y = 0; y < this.rows; y++) {
         var tile = UILayer({
           x: (x * this.tileWidth),
           y: (y * this.tileHeight),
           width: this.tileWidth,
-          height: this.tileHeight,
+          height: this.tileHeight
         });
-        if (this.map.sublayers.length == 0) {
-          tile.backgroundColor = '#FFF4D9';
-        };
+        // if (this.map.sublayers.length == 0) {
+        //   tile.backgroundColor = '#FFF4D9';
+        // };
         var self = this;
         tile.style.border = '1px solid #ccc';
-        this.map.sublayers.getLast().addSublayer(tile);
-      };
-    };
+        layer.addSublayer(tile);
+      }
+    }
   },
   attach: function(elem) {
     elem.grab(this.map.element);
@@ -74,20 +99,20 @@ var Cartog = new Class({
       });
     });
   },
-  export: function() {
+  exportMap: function() {
     var self = this;
-    var dict = {};
-    dict["attributes"] = {
+    var map = {};
+    map.settings = {
       width: self.width,
       height: self.height,
       rows: self.rows,
       columns: self.columns
-    }
+    };
     Array.each(self.map.sublayers, function(layer, index) {
-      tiles = self.coordinatesForLayer(layer);
-      dict[layer.tag] = tiles;
+      var tiles = self.coordinatesForLayer(layer);
+      map[layer.tag] = tiles;
     });
-    return JSON.stringify(dict);
+    return JSON.stringify(map);
   },
   coordinatesForLayer: function(layer) {
     var self = this;
@@ -99,7 +124,7 @@ var Cartog = new Class({
           y:tile.frame.y / self.tileHeight,
           color:tile.tag
         }]);
-      };
+      }
     });
     return tiles;
   },
@@ -118,27 +143,27 @@ var Cartog = new Class({
   open: function(file, contents) {
     var self = this;
     self.name = file.name;
-    var dict = JSON.parse(contents);
-    var attr = dict["attributes"];
-    delete dict["attributes"];
-    self.width = attr["width"];
-    self.height = attr["height"];
-    self.columns = attr["columns"];
-    self.rows = attr["rows"];
+    var map = JSON.parse(contents);
+    var settings = map.settings;
+    delete map.settings;
+    self.width = settings.width;
+    self.height = settings.height;
+    self.columns = settings.columns;
+    self.rows = settings.rows;
     self.map.removeAllSublayers();
     self.map.frame.width = self.width;
     self.map.frame.height = self.height;
     self.updateTileSize();
-    for (var key in dict) {
+    for (var key in map) {
       self.addLayer(key);
-      Array.each(dict[key], function(tile) {
+      Array.each(map[key], function(tile) {
         self.selectedColor = tile.color;
         var index = (self.rows * tile.x) + tile.y;
         var newTile = self.map.layerWithTag(key).sublayers[index];
         newTile.backgroundColor = tile.color;
         newTile.tag = tile.color;
       });
-    };
+    }
   }
 });
 
@@ -155,29 +180,40 @@ var CartogUI = new Class({
     this.fileInput = document.getElementById('fileInput');
     this.saveButton = document.getElementById('save');
     this.colorValue = document.getElementById('color_value');
+    this.widthSetting = document.getElementById('width');
+    this.heightSetting = document.getElementById('height');
+    this.rowSetting = document.getElementById('rows');
+    this.columnSetting = document.getElementById('columns');
+    this.tools = document.getElementById('tools');
+    this.mapHolder = document.getElementById('map');
     this.name = document.getElementById('name');
     this.mimeType = 'application/json';
-    this.format = $('format');
+    this.format = document.getElementById('format');
     this.updateLayerList();
     this.updateColorList();
+    this.layoutViews();
+    this.bindEvents();
+  },
+  bindEvents: function() {
     this.bindLayerEvents();
     this.bindLayerButtons();
     this.bindFileEvents();
     this.bindColorEvents();
+    this.bindSettingsEvents();
   },
   addLayerToList: function(name) {
     var exists = false;
     Array.each(this.layerList.getChildren(), function(li) {
       if (li.getElement('a').text.toLowerCase() === name) {
         exists = true;
-      };
+      }
     });
     if (!exists) {
       var new_layer = this.layerList.getLast('li').clone();
       new_layer.getElement('a').addClass('success');
       new_layer.getElement('a').set('text', name);
       new_layer.injectBefore(this.layerList.getFirst('li'));
-    };
+    }
   },
   bindLayerButtons: function() {
     var self = this;
@@ -201,8 +237,8 @@ var CartogUI = new Class({
         'class': 'label span3',
         styles: {
           backgroundColor: color
-        },
-      })
+        }
+      });
       content.addEvent('click', function() {
         self.cartogMap.setSelectedColor(color);
       });
@@ -228,7 +264,7 @@ var CartogUI = new Class({
         self.addLayerToList(self.layerName.value);
         self.layerName.value = '';
         self.bindLayerButtons();
-      };
+      }
     });
   },
   cleanUpSave: function(a) {
@@ -251,7 +287,7 @@ var CartogUI = new Class({
     // }
 
     var bb = new BlobBuilder();
-    bb.append(this.cartogMap.export());
+    bb.append(this.cartogMap.exportMap());
 
     self.exportButton.download = self.name.value + self.format.value;
     self.exportButton.href = window.URL.createObjectURL(bb.getBlob(self.mimeType));
@@ -285,7 +321,7 @@ var CartogUI = new Class({
         self.cartogMap.addColor('#' + self.colorValue.value);
         self.colorValue.value = '';
         self.updateColorList();
-      };
+      }
     });
   },
   bindFileEvents: function() {
@@ -296,6 +332,7 @@ var CartogUI = new Class({
       self.saveState.getElement('p').set('text', 'Saved! Ready to export.');
       self.downloadFile();
       self.exportButton.removeClass('disabled');
+      self.disableSettings();
     });
 
     self.fileInput.addEvent('change', function(evt) {
@@ -316,13 +353,53 @@ var CartogUI = new Class({
       });
     });
   },
+  disableSettings: function() {
+    var self = this;
+    self.widthSetting.addClass('disabled').set('disabled', true);
+    self.heightSetting.addClass('disabled').set('disabled', true);
+    self.rowSetting.addClass('disabled').set('disabled', true);
+    self.columnSetting.addClass('disabled').set('disabled', true);
+  },
+  layoutViews: function() {
+    var self = this;
+    self.mapHolder.setStyle('width', self.cartogMap.width + 'px');
+    self.tools.setStyle('left', self.mapHolder.getCoordinates().right + 80 + 'px');
+  },
+  bindSettingsEvents: function() {
+    var self = this;
+    self.widthSetting.addEvent('keyup', function() {
+      if (this.value !== '') {
+        self.cartogMap.setWidth(this.value);
+        self.cartogMap.bindLayerEvents();
+        self.layoutViews();
+      }
+    });
+    self.heightSetting.addEvent('keyup', function() {
+      if (this.value !== '') {
+        self.cartogMap.setHeight(this.value);
+        self.cartogMap.bindLayerEvents();
+        self.layoutViews();
+      }
+    });
+    self.rowSetting.addEvent('keyup', function() {
+      if (this.value !== '') {
+        self.cartogMap.setRows(this.value);
+        self.cartogMap.bindLayerEvents();
+        self.layoutViews();
+      }
+    });
+    self.columnSetting.addEvent('keyup', function() {
+      if (this.value !== '') {
+        self.cartogMap.setColumns(this.value);
+        self.cartogMap.bindLayerEvents();
+        self.layoutViews();
+      }
+    });
+  }
 });
 
-var sidebar_width = $('map').getCoordinates()['left'];
-var align_to_form = $('settings').getElement('h6').getCoordinates()['bottom'];
+var sidebar_width = $('map').getCoordinates().left;
+var align_to_form = $('settings').getElement('h6').getCoordinates().bottom;
 var cartog = new Cartog(sidebar_width, align_to_form);
 cartog.attach($('map'));
 var cartogUI = new CartogUI(cartog);
-
-$('map').setStyle('width', cartog.map.frame.width);
-$('tools').setStyle('left', $('map').getCoordinates()['right'] + 80 + 'px');
